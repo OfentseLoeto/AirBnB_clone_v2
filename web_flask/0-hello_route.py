976160c9b328d61de63efa8107d59472877a6fb7
ofentse loeto <ohentseloeto610@gmail.com>
2023-08-17 08:37:48 +0000

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,6 +4,7 @@ starts a Flask web application
 """
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
@@ -13,4 +14,4 @@ def index():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
